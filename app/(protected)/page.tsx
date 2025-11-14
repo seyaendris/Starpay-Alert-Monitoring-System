d266358613c5355b29bf87bc9a561eff07584c22
seyaendris <seide9894@gmail.com>
2025-11-14 15:23:32 +0300

    Refactor protected page: add loading states

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
-     Project Started
+    <div className="">
+     Alerts
     </div>
   );
 }
