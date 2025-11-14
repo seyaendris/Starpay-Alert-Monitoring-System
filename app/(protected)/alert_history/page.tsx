2e560e87fe4526b66d8387c1fb6b80ac7facff4f
seyaendris <seide9894@gmail.com>
2025-11-14 15:24:45 +0300

    Implement alert history page with filtering

---
+++ app/(protected)/alert_history/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlertHistoryPage = () => {
+  return (
+    <div>
+        {/* <AlertHistoryTable /> */}
+        </div>
+  )
+}
+
+export default AlertHistoryPage
